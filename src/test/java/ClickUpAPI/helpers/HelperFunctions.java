a0a5ade00a147d35751f78a0f6cae5a6540725fb
kristelTDL <kristel.juurik@testdevlab.com>
2023-11-05 19:48:10 +0200

    correct file

--- src/test/java/ClickUpAPI/helpers/HelperFunctions.java
+++ src/test/java/ClickUpAPI/helpers/HelperFunctions.java
@@ -4,21 +4,6 @@ import java.nio.charset.Charset;
 import java.util.Random;
 
 public class HelperFunctions {
-<<<<<<< HEAD
-    public static String randomString() {
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
-        Random random = new Random();
-
-        String generatedString = random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-
-        return generatedString;
-=======
     /**
      * A function to help generate a random string.
      * Generates random numbers between 48 to 122 to match ASCII codes for letters and numbers.
